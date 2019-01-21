be196a03ba4c857b6db0f3dd9a4d91ec5c123961
yankee <koluchaw@gmail.com>
2019-01-21 17:05:51 +0630

    'ff'

--- first/urls.py
+++ first/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^dashboard', views.index, name='index'),
-    url(r'^attended', views.attended, name='attended'),
+    url(r'^attended$', views.attended, name='attended'),
+    url(r'^attended/add', views.add, name='add'),
+
 ]
--- first/views.py
+++ first/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
 from django.template import loader
 from first.models import Attended
 from django.contrib.auth.models import User
-import datetime
+from datetime import timedelta, datetime, tzinfo
+import json
+import time
+from django.utils import timezone
 
 
 # Create your views here.
@@ -23,6 +27,15 @@ def attended(request):
         if Att.count() != 0:
             user.att = Attended.objects.get(id=user.id)
         else:
-            user.att = 'd'
+            user.att = ''
 
     return HttpResponse(template.render({'users': users, 'data_att': data_att}))
+
+
+def add(request):
+    # ts = json.loads(request.body).get('timestamp')
+    # userid = json.loads(request.body).get('user_id')
+    # readable = datetime.fromtimestamp(ts / 1000)
+    a = Attended.objects.create(user_id=1, start=33, end='', status='fff')
+    a.save()
+    return JsonResponse({'time': 'aa'})
